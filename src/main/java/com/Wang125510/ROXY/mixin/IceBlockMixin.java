797852ee0f0b506056b3c3efb7993113b735482d
Wang125510 <wang125510@outlook.com>
2026-01-09 01:02:41 +0800

    修改IceBreakMixin

--- src/main/java/com/Wang125510/ROXY/mixin/IceBlockMixin.java
+++ src/main/java/com/Wang125510/ROXY/mixin/IceBlockMixin.java
@@ -1,24 +1,18 @@
 package com.Wang125510.ROXY.mixin;
 
 import com.Wang125510.ROXY.Rules;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.level.block.IceBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(IceBlock.class)
 public class IceBlockMixin {
-	@WrapOperation(method = "playerDestroy",
+	@ModifyExpressionValue(method = "playerDestroy",
 			at = @At(value = "INVOKE",
 					target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;hasTag(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/tags/TagKey;)Z"))
-	private boolean forceGlobalSilkTouch(ItemStack itemStack, TagKey<Enchantment> tagKey, Operation<Boolean> original) {
-		if (Rules.alwaysSilkTouch) {
-			return true;
-		}
-		return original.call(itemStack, tagKey);
+	private boolean forceGlobalSilkTouch(boolean original) {
+		if (Rules.alwaysSilkTouch) { return true; }
+		return original;
 	}
 }
